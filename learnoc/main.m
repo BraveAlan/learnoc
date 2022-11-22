e76713db232b973dce0c53df3f8cad41cf5fda1f
“josephfang” <josephfang@tencent.com>
2022-11-22 11:50:09 +0800

    Initial Commit

---
+++ learnoc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  learnoc
+//
+//  Created by josephfang on 2022/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+// test注释
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
